be76f31e2afc8056ae3a8c93e0258ef41a8a5a7d
Raphael Barreto <tnt.raphael@gmail.com>
2021-03-23 19:38:15 -0300

    Pages & Routes

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error minima
+        sunt dicta eius dolore quaerat eum iste odit ratione expedita,
+        asperiores debitis, ex voluptas mollitia corrupti distinctio, numquam
+        unde sit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error minima
+        sunt dicta eius dolore quaerat eum iste odit ratione expedita,
+        asperiores debitis, ex voluptas mollitia corrupti distinctio, numquam
+        unde sit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error minima
+        sunt dicta eius dolore quaerat eum iste odit ratione expedita,
+        asperiores debitis, ex voluptas mollitia corrupti distinctio, numquam
+        unde sit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error minima
+        sunt dicta eius dolore quaerat eum iste odit ratione expedita,
+        asperiores debitis, ex voluptas mollitia corrupti distinctio, numquam
+        unde sit.
+      </p>
+    </div>
+  )
+}
