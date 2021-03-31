f7bb4c61d99258ccbdc292fd0d15a59b9a6053ea
Raphael Barreto <tnt.raphael@gmail.com>
2021-03-31 15:34:34 -0300

    Removed fragment

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,12 @@ import Navbar from './Navbar';
 
 export default function Layout({ children }) {
   return (
-    <>
-      <div className="layout">
-        <Navbar />
-      </div>
+    <div className="layout">
+      <Navbar />
       <div className="content">{children}</div>
       <footer>
-        <p>Copyright 2021 Web Warrior &copy;</p>
+        <p>Copyright 2021 Web Warrior</p>
       </footer>
-    </>
+    </div>
   );
 }
