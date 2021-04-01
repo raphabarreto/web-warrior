e1645d6d241890f02ff1b64dca25bb7f12bec796
Raphael Barreto <tnt.raphael@gmail.com>
2021-03-31 21:55:30 -0300

    Multiple Queries

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -4,7 +4,8 @@ import Layout from '../../components/Layout';
 import { portfolio, project } from '../../styles/projects.module.css';
 
 export default function Projects({ data }) {
-  const projects = data.allMarkdownRemark.nodes;
+  const projects = data.projects.nodes;
+  const contact = data.contact.siteMetadata.contact;
 
   return (
     <Layout>
@@ -21,6 +22,7 @@ export default function Projects({ data }) {
             </Link>
           ))}
         </div>
+        <p>Like what you see? Email me at {contact} from a quote!</p>
       </div>
     </Layout>
   );
@@ -29,7 +31,9 @@ export default function Projects({ data }) {
 // export page query
 export const query = graphql`
   query ProjectsPage {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+    projects: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
       nodes {
         frontmatter {
           slug
@@ -37,6 +41,11 @@ export const query = graphql`
           title
         }
         id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
       }
     }
   }
