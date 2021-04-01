0e933592749ffaa132d2fadd69d402c6c1fa9918
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-01 00:38:33 -0300

    Query Variables

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import { details, html, featured } from '../styles/project-details.module.css';
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark;
+
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter;
+
   return (
     <Layout>
       <div className={details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={featured}>
-          {/* <GatsbyImage
-            image={project.frontmatter.thumb.childImageSharp.gatsbyImageData}
-            alt={`Project - ${project.frontmatter.title}`}
-          /> 
-          <div className={html} dangerouslySetInnerHTML={} />
-          */}
+          <GatsbyImage
+            image={featuredImg.childImageSharp.gatsbyImageData}
+            alt={`Project - ${title}`}
+          />{' '}
+          <div className={html} dangerouslySetInnerHTML={{ __html: html }} />
         </div>
       </div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`;
